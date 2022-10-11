405190cab808da19283b603e3a2556c911de30da
坏黑 <b@skymc.me>
2022-10-11 13:09:20 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
-tasks.withType<KotlinCompile> {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
         freeCompilerArgs = listOf("-Xjvm-default=all")
